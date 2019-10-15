6d4a29a9c02c5e23a195619da34c2d7132660d52
Daybson <kaz-29@hotmail.com>
2019-10-15 19:15:10 -0300

    + Criação do DatabaseBuilder + Criação do banco de dados via código

---
+++ UnitySQLiteMultiplataforma/Assets/Scripts/DatabaseBuilder.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mono.Data.Sqlite;
+using System.IO;
+
+public class DatabaseBuilder : MonoBehaviour
+{
+    public string DatabaseName;
+    protected string databasePath;
+    protected SqliteConnection Connection => new SqliteConnection($"Data Source = {this.databasePath};");
+
+    private void Awake()
+    {
+        if (string.IsNullOrEmpty(this.DatabaseName))
+        {
+            Debug.LogError("Database name is empty!");
+            return;
+        }
+
+        CreateDatabaseFileIfNotExists();
+    }
+
+    private void CreateDatabaseFileIfNotExists()
+    {
+        this.databasePath = Path.Combine(Application.persistentDataPath, this.DatabaseName);
+
+        if (!File.Exists(this.databasePath))
+        {
+            SqliteConnection.CreateFile(this.databasePath);
+            Debug.Log($"Database path: {this.databasePath}");
+        }
+    }
+}
