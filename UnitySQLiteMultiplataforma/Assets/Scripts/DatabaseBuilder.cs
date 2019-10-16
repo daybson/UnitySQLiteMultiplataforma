7a243875019b3d08f9f319beb17e4e03279be212
Daybson <kaz-29@hotmail.com>
2019-10-15 21:46:50 -0300

    * DatabaseBuilder: criado método de cópia do database e coroutine de cópia para Android/WebGL + Cena de teste dos paths do dispositivo

--- UnitySQLiteMultiplataforma/Assets/Scripts/DatabaseBuilder.cs
+++ UnitySQLiteMultiplataforma/Assets/Scripts/DatabaseBuilder.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Mono.Data.Sqlite;
 using System.IO;
+using UnityEngine.Networking;
 
 public class DatabaseBuilder : MonoBehaviour
 {
@@ -17,8 +18,48 @@ public class DatabaseBuilder : MonoBehaviour
             Debug.LogError("Database name is empty!");
             return;
         }
+        CopyDatabaseFileIfNotExists();
+        //CreateDatabaseFileIfNotExists();
+    }
 
-        CreateDatabaseFileIfNotExists();
+    private void CopyDatabaseFileIfNotExists()
+    {
+        this.databasePath = Path.Combine(Application.persistentDataPath, this.DatabaseName);
+
+        if (File.Exists(this.databasePath))
+            return;
+
+        var originDatabasePath = string.Empty;
+        var isAndroid = false;
+
+#if UNITY_EDITOR || UNITY_WP8 || UNITY_WINRT
+
+        originDatabasePath = Path.Combine(Application.streamingAssetsPath, this.DatabaseName);
+
+#elif UNITY_STANDALONE_OSX
+
+        originDatabasePath = Path.Combine(Application.dataPath, "/Resources/Data/StreamingAssets/", this.DatabaseName);
+        
+#elif UNITY_IOS
+
+        originDatabasePath = Path.Combine(Application.dataPath, "Raw", this.DatabaseName);        
+
+#elif UNITY_ANDROID
+
+        isAndroid = true;
+        originDatabasePath = "jar:file://" + Application.dataPath + "!/assets" + this.DatabaseName;
+        StartCoroutine(GetInternalFileAndroid(originalDatabasePath));
+
+#elif UNITY_WEBGL
+
+        isAndroid = true;
+        originDatabasePath = Path.Combine(Application.streamingAssetsPath, this.DatabaseName);
+        StartCoroutine(GetInternalFileAndroid(originalDatabasePath));
+
+#endif
+
+        if (!isAndroid)
+            File.Copy(originDatabasePath, this.databasePath);
     }
 
     private void CreateDatabaseFileIfNotExists()
@@ -31,4 +72,22 @@ public class DatabaseBuilder : MonoBehaviour
             Debug.Log($"Database path: {this.databasePath}");
         }
     }
+
+
+    private IEnumerator GetInternalFileAndroid(string path)
+    {
+        var request = UnityWebRequest.Get(path);
+        yield return request.SendWebRequest();
+
+        if (request.isHttpError || request.isNetworkError)
+        {
+            Debug.LogError($"Error reading android file!: {request.error}");
+        }
+        else
+        {
+            File.WriteAllBytes(this.databasePath, request.downloadHandler.data);
+            Debug.Log("File copied!");
+        }
+    }
+
 }
---
+++ UnitySQLiteMultiplataforma/Assets/Scripts/PathTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PathTest : MonoBehaviour
+{
+    public Text text;
+
+    void Start()
+    {
+        text.text = $"DATA PATH: {Application.dataPath}";
+        text.text += $"\n\nPERSISTENT DATA PATH: {Application.persistentDataPath}";
+        text.text += $"\n\nSTREAMING ASSETS PATH: {Application.streamingAssetsPath}";
+    }
+
+}
