0c3515a6691921196893dd010ee4fdb56bafdce3
Daybson <kaz-29@hotmail.com>
2019-11-09 17:00:06 -0300

    + GameController: teste de acesso com Singleton do GamesCodeDataSource + GamesCodeDataSource: classe que herda de SQLiteDataSource e instancia os objetos DAO

---
+++ UnitySQLiteMultiplataforma/Assets/Scripts/Persistence/DataSources/GamesCodeDataSource.cs
@@ -0,0 +1,51 @@
+﻿using Assets.Scripts.Persistence.DAO.Implementation;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamesCodeDataSource : SQLiteDataSource
+{
+    public CharacterDAO CharacterDAO { get; protected set; }
+    public WeaponDAO WeaponDAO { get; protected set; }
+
+    private static GamesCodeDataSource instance;
+    public static GamesCodeDataSource Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<GamesCodeDataSource>();
+                
+                if (instance == null)
+                {
+                    var gamesCodeDataSourceObject = new GameObject("GamesCodeDataSource");
+                    instance = gamesCodeDataSourceObject.AddComponent<GamesCodeDataSource>();
+                    DontDestroyOnLoad(gamesCodeDataSourceObject);
+                }
+            }
+
+            return instance;
+        }
+    }
+
+    protected void Awake()
+    {
+        this.databaseName = "GamesCode.db";
+        this.copyDatabase = true;
+
+        try
+        {
+            base.Awake();
+            CharacterDAO = new CharacterDAO(this);
+            WeaponDAO = new WeaponDAO(this);
+        }
+        catch (Exception ex)
+        {
+            Debug.LogError($"Database not created! {ex.Message}");
+        }
+
+        print("Awake GamesCode");
+    }
+}
