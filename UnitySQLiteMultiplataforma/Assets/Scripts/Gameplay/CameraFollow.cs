f04b7c2093b7ec89e21bbc071d646650efe69384
Daybson <kaz-29@hotmail.com>
2019-12-18 23:39:13 -0300

    * mudando camera follow

--- UnitySQLiteMultiplataforma/Assets/Scripts/Gameplay/CameraFollow.cs
+++ UnitySQLiteMultiplataforma/Assets/Scripts/Gameplay/CameraFollow.cs
@@ -4,38 +4,17 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform Character;
-    public float distance;
-    public float heightY;
+    public Transform target;
+    public Vector3 distance;
+    public float speed;
 
-    public float speedRotation;
-    public float speedHeight;
-
-
-    void Start()
+    private void Update()
     {
+        transform.position = Vector3.Lerp(
+            transform.position,
+            target.position + distance,
+            speed * Time.deltaTime);
         
-    }
-
-   void Update()
-    {
-        float currentYAngle = transform.eulerAngles.y;
-        float desiredYAngle = Character.transform.eulerAngles.y;
-
-        float currentHeight = transform.position.y;
-        float desiredHeight = Character.transform.position.y + heightY;
-
-
-        currentYAngle = Mathf.LerpAngle(currentYAngle, desiredYAngle, speedRotation * Time.deltaTime);
-        currentHeight = Mathf.Lerp(currentHeight, desiredHeight, speedHeight * Time.deltaTime);
-
-        Quaternion currentRotation = Quaternion.Euler(0, currentYAngle, 0);
-
-        Vector3 relativePosition = Character.position;
-        Vector3 orbitalPosition = currentRotation * -Vector3.forward * distance;
-        relativePosition += orbitalPosition;
-
-        transform.position = new Vector3(relativePosition.x, currentHeight, relativePosition.z);
-        transform.LookAt(Character);
+        transform.LookAt(target);
     }
 }
