d1f672bd7dc036d1496c101161870b5c6818281f
Daybson B. Salles Paisante <daybson.paisante@outlook.com>
2021-01-07 22:45:09 -0300

    + Classe de scripts de migração

---
+++ NovoProjetoSQLite/Assets/Scripts/Persistence/SQLMigration.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Assets.Scripts.Persistence
+{
+    public class SQLMigration
+    {
+        public readonly static Dictionary<int, List<string>> versionScripts = new Dictionary<int, List<string>>()
+        {
+            { 
+                2, 
+                new List<string>()
+                {
+                    "UPDATE Weapon SET Attack = 35 WHERE Id = 1;",
+                    "DELETE FROM Character WHERE Id = 1;",
+                }
+            },
+        };
+    }
+}
